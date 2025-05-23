ce44a0150e92d2e1914c145ba6039678cf1acab7
munal777 <munalpoudel3@gmail.com>
2025-05-23 08:48:32 +0545

    added the question name comment on other solved code of sql file

--- sql/175.sql
+++ sql/175.sql
@@ -1,2 +1,3 @@
+-- 175. Combine Two Tables
 -- Write your PostgreSQL query statement below
 select firstName, lastName, city, state from person left join address on address.personid = person.personid;
