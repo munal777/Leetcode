2459a5df9c0a5a66491096262625a7da3308ca74
munal777 <munalpoudel3@gmail.com>
2025-05-25 21:59:59 +0545

    started solving the 2131 question

---
+++ python/2131.py
@@ -0,0 +1,8 @@
+# 2131. Longest Palindrome by Concatenating Two Letter Words
+
+class Solution(object):
+    def longestPalindrome(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
