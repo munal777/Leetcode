7c43f3deb57b6dd938d9beadba5a7e97f6a03340
munal777 <munalpoudel3@gmail.com>
2025-05-27 22:22:03 +0545

    question num 2894

--- python/2894.py
+++ python/2894.py
@@ -1,1 +1,17 @@
 # 2894. Divisible and Non-divisible Sums Difference
+
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        divisible_sum = 0
+        non_divisible_sum = 0
+
+        for num in range(1,n+1):
+            if num%m == 0:
+                divisible_sum += num
+            else:
+                non_divisible_sum += num
