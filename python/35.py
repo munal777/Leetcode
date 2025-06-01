d188804d974bf2909d7ab80a5b0523c46d1e8bd6
munal777 <munalpoudel3@gmail.com>
2025-06-01 21:38:18 +0545

    solved the ques num 35

--- python/35.py
+++ python/35.py
@@ -8,4 +8,17 @@ class Solution(object):
         :rtype: int
         """
 
-        
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
+
+        return low
