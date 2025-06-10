864ef4cce857d23fee1631c2560c9b80507ebb60
munal777 <munalpoudel3@gmail.com>
2025-06-10 22:51:12 +0545

    solved the question num 125

--- python/125.py
+++ python/125.py
@@ -1,1 +1,17 @@
 # 125. Valid Palindrome
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        valid_s = ""
+        
+        for char in s:
+            if char.isalpha():
+                valid_s += char.lower()
+            if char.isnumeric():
+                valid_s += char
+        return True if valid_s[0:len(valid_s)] == valid_s[::-1] else False
