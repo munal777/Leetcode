0ce23c42f101ad70b2bc3937c6e3a0e786c3a850
munal777 <munalpoudel3@gmail.com>
2025-06-24 10:51:46 +0545

    started solving today ques

---
+++ python/2200.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findKDistantIndices(self, nums, key, k):
+        """
+        :type nums: List[int]
+        :type key: int
+        :type k: int
+        :rtype: List[int]
+        """
+
+
+
+
+
+obj = Solution()
+nums = [3,4,9,1,3,9,5]
+key = 9
+k = 1
+print(obj.findKDistantIndices(nums, key, k))
