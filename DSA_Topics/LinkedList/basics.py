8d44e82bbaa4d3d6c4378a7c662b4766940bbe2d
munal777 <munalpoudel3@gmail.com>
2025-08-24 13:07:50 +0545

    chore: learn the traversal in linkedlist

--- DSA_Topics/LinkedList/basics.py
+++ DSA_Topics/LinkedList/basics.py
@@ -4,3 +4,24 @@ class Node:
        # Data part of the node
         self.data = data   
         self.next = None
+
+def traverseList(head):
+    if head is None:
+        print()
+        return
+    
+    print(head.data, end=' ')
+
+    traverseList(head.next)
+
+def main():
+
+    head = Node(10)
+    head.next = Node(20)
+    head.next.next = Node(30)
+
+    traverseList(head)
+
+if __name__ == "__main__":
+    main()
+
