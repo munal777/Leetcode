a9ff8e26d5521814be85f66d4af8a6126548ed93
munal777 <munalpoudel3@gmail.com>
2025-08-26 19:11:50 +0545

    refactor: the next is not defined properly

--- DSA_Topics/LinkedList/insertion.py
+++ DSA_Topics/LinkedList/insertion.py
@@ -2,5 +2,21 @@ class Node:
     
     def __init__(self, data):
         self.data = data
-        self.next = next
-        
+        self.next = None
+    
+def print_list(head):
+
+    while head is not None:
+        print(head.data)
+
+        head = head.next
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+
+    print_list(head)
+
+if __name__ == '__main__':
+    main()
