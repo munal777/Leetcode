84d62bf8e4ebefdcef4803d86c7eb8ad034b4f74
munal777 <munalpoudel3@gmail.com>
2026-02-13 23:12:09 +0545

    feat: start the recap

---
+++ Recap/1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        for i in range(0,len(nums)):
+            print(nums[i])
+
+
+nums = [2,7,11,15]
+obj = Solution()
+obj.twoSum(nums, 2)
