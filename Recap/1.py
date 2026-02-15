22cb7597588cd6a6fc903ac5966fe1652e628f97
munal777 <munalpoudel3@gmail.com>
2026-02-15 23:39:55 +0545

    chore: solve with the o(n) time complexity

--- Recap/1.py
+++ Recap/1.py
@@ -5,11 +5,30 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # --- O(n2) time complexity approach ---
+        # for i in range(0,len(nums) - 1):
+        #     for j in range(i+1, len(nums)):
+        #         sums = nums[i] + nums[j]
+        #         if sums == target:
+        #             return [i,j]
+
+        # return []
+
+        # ---  O(n) time complexity approach ---
+        maps = {}
+
+        for index in range(0, len(nums)):
+            curr_num = nums[index]
+            complement_num = target - curr_num
+            if  complement_num in maps:
+                return [maps[complement_num], index]
+            
+            maps[curr_num] = index
         
-        for i in range(0,len(nums)):
-            print(nums[i])
+        return []
+
 
 
 nums = [2,7,11,15]
 obj = Solution()
-obj.twoSum(nums, 2)
+print(obj.twoSum(nums, 9))
