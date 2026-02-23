9c887aa6d8444ca6e456fd05f44303315794ebfc
munal777 <munalpoudel3@gmail.com>
2026-02-23 18:51:04 +0545

    practise the fizzbuzz ques

---
+++ Recap/basics_topics/FizzBuzz.py
@@ -0,0 +1,9 @@
+def check_fizz_buzz(num):
+    if num%3 == 0 and num%5 == 0:
+        print("FizzBuzz")
+    elif num%3 == 0:
+        print("Fizz")
+    elif num%5 == 0:
+        print("Buzz")
+
+check_fizz_buzz(30)
